cfd678aa84d50e3c0cf70d533d9f75b9028b244c
Leysreng ol <leysreng.ol@student.passerellesnumeriques.org>
2024-04-26 19:14:08 +0700

    pilotFlight

--- src/Pilot.ts
+++ src/Pilot.ts
@@ -1,7 +1,13 @@
 import { Employee } from "./Employee";
 import { Flight } from "./Flight";
 
-class Pilot extends Employee {
-    private flight:Flight;
-    
+export class Pilot extends Employee {
+    private flight:Flight[]=[];
+    constructor( salary:string,firstName:string,lastName:string,contecctInFo:string){
+        super(salary,firstName,lastName,contecctInFo);
+    }
+ getAllFlight(flight:Flight):Flight[]{
+    for(let )
+
+ }
 }
