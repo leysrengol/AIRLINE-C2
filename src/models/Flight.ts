1cc063635a2bf7c0ab0a41b7ad479e29d858cfa5
LIEP.TOEUR <LIEP.TOEUR@pnc.lan>
2024-04-30 17:08:00 +0700

    [fix]

--- src/models/Flight.ts
+++ src/models/Flight.ts
@@ -2,26 +2,17 @@ import { Passanger } from "./Passanger";
 import { Pilot } from "../controllers/Pilot";
 import { Router } from "./Route";
 import { Seat } from "./Seat";
-<<<<<<< HEAD
 import { Gate } from "./Gate";
 import { Airline } from "./Airline";
 import { Airport } from "./Airport";
-=======
-import { DateTime } from "./DateTime";
->>>>>>> 8e3fd41e25a975d735ffb5cdbf8ba296cce08af1
 
 export class Flight {
   private seats: Seat[] = [];
   private router: Router;
   private passengers: Passanger[];
-<<<<<<< HEAD
   private airline: Airline;
   private airport: Airport;
   private gate: Gate;
-=======
-  private pilot: Pilot;
-
->>>>>>> 8e3fd41e25a975d735ffb5cdbf8ba296cce08af1
 
   constructor(
     private flightNumber: string,
